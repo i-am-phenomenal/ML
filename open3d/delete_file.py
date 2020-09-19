7ad1b1dbe78b92c0348d784f9069129c0d6bdcdc
Aditya Chaturvedi <chat29aditya@gmail.com>
2020-09-20 01:26:24 +0530

    Added util for folder oprns

--- open3d/delete_file.py
+++ open3d/delete_file.py
@@ -1,24 +1,38 @@
 import os 
 import glob
+import shutil
 
-filePath = "C:\Code\Images_DataSet\From_VLC\chair_dataset_2\chairImages/"
-matFiles = [file for file in os.listdir(filePath) if file.endswith('.mat')]
-if matFiles: 
-    for matFile in matFiles: 
-        try: 
-            os.remove(filePath + matFile)
-        except Exception as e:
-            print(e)
-    print("Deleted all files")
+def deleteFilesWithGivenExtension(extension):
+    filePath = "C:\Code\Images_DataSet\From_VLC\chair_dataset_2\chairImages/"
+    matFiles = [file for file in os.listdir(filePath) if file.endswith(extension)]
+    if matFiles: 
+        for matFile in matFiles: 
+            try: 
+                os.remove(filePath + matFile)
+            except Exception as e:
+                print(e)
+        print("Deleted all files")
 
-siftFiles = [file for file in os.listdir(filePath) if file.endswith('.sift')]
-if siftFiles:
-    for siftFile in siftFiles: 
-        try: 
-            os.remove(filePath + siftFile)
-        except Exception as e:
-            print(e)
-    print("Deleted all files")
+# deleteFilesWithGivenExtension('.sift')
+# deleteFilesWithGivenExtension('.mat')
 
-allFiles = [file for file in os.listdir(filePath)]
-print(len(allFiles))
+def moveFilesToGivenFolder(source, destination):
+    allFiles = os.listdir(source)
+    for file in allFiles: 
+        shutil.move(source + "/" + file, destination)
+    print("Moved all files")
+
+def deleteFolder(dirPath): 
+    os.rmdir(dirPath)
+    print("Deleted Dir")
+
+source = "C:/Code/Images_DataSet/From_VLC/chair_dataset_2/"
+destination = "C:/Code/Images_DataSet/From_VLC/chair_dataset_2/Mug_0"
+for iter in range(1, 9):
+    completeSource = source + "Mug_" + str(iter)
+    # moveFilesToGivenFolder(completeSource, destination)
+    deleteFolder(completeSource)
+
+
+
+    
